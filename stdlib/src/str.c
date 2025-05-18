33bf9fc6f524ae6b1ceb92c371bce35fccc4fd19
TypicaIity <alboxqs.return@gmail.com>
2025-05-18 12:11:15 +0300

    Add 'str::atoi'

--- stdlib/src/str.c
+++ stdlib/src/str.c
@@ -110,6 +110,29 @@ char* _str__cat(int count, ...) {
 	return buffer;
 }
 
+int _str__atoi(const char* str) {
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r' || str[i] == '\f' || str[i] == '\v')
+		i++;
+
+	if (str[i] == '+' || str[i] == '-') {
+		if (str[i] == '-') {
+			sign = -1;
+		}
+		i++;
+	}
+
+	while (str[i] >= '0' && str[i] <= '9') {
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+
+	return sign * result;
+}
+
 #else
 #error "'str' is not yet available on non-windows machines"
 #endif
